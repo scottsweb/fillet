38fbb63af7826f5e20344450a5c9becefd69c05a
Scott Evans <git@scott.ee>
2014-08-13 19:59:36 +0100

    First pass. Shortcode needs some work.

---
+++ assets/js/fillet-mce.js
@@ -0,0 +1,63 @@
+(function() {
+	tinymce.PluginManager.add('fillet', function( editor, url ) {
+		editor.addButton( 'fillet', {
+			icon: false,
+			type: 'button',
+			title: 'iFrame Embed',
+			onclick: function() {
+				editor.windowManager.open( {
+					title: 'iFrame Embed',
+					width: 600,
+					height: 150,
+					body: [
+						{
+							type: 'textbox',
+							name: 'url',
+							label: 'iFrame URL',
+							value: 'http://',
+							autofocus: true
+						},
+						{
+							type: 'textbox',
+							name: 'width',
+							label: 'Width (px)',
+							value: '700',
+							size: 4,
+							maxLength: 4
+						},
+						{
+							type: 'textbox',
+							name: 'height',
+							label: 'Height (px)',
+							value: '300',
+							size: 4,
+							maxLength: 4
+						}
+					],
+					onsubmit: function( e ) {
+
+						var shortcode = '<p>[iframe'
+
+						// iframe url
+						if (e.data.url != '') {
+							shortcode += ' url="' + e.data.url + '"';
+						}
+
+						// width
+						if (e.data.width != '') {
+							shortcode += ' width="' + e.data.width + '"';
+						}
+
+						// width
+						if (e.data.height != '') {
+							shortcode += ' height="' + e.data.height + '"';
+						}
+
+						shortcode += ']</p>';
+						editor.insertContent( shortcode );
+					}
+				});
+			}
+		});
+	});
+})();
---
+++ fillet.php
@@ -0,0 +1,195 @@
+<?php
+/*
+Plugin Name: Fillet
+Plugin URI: https://bitbucket.com/cftp/fillet
+Description: Safely embed iFrames in the WordPress text editor.
+Version: 1.0
+Author: Scott Evans (Code For The People)
+Author URI: http://codeforthepeople.com
+Text Domain: fillet
+Domain Path: /assets/languages/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Copyright © 2014 Code for the People ltd
+
+                _____________
+               /      ____   \
+         _____/       \   \   \
+        /\    \        \___\   \
+       /  \    \                \
+      /   /    /          _______\
+     /   /    /          \       /
+    /   /    /            \     /
+    \   \    \ _____    ___\   /
+     \   \    /\    \  /       \
+      \   \  /  \____\/    _____\
+       \   \/        /    /    / \
+        \           /____/    /___\
+         \                        /
+          \______________________/
+
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+class fillet {
+
+	/**
+	* fillet
+	*
+	* @author Scott Evans
+	*/
+	function __construct() {
+
+		add_shortcode( 'fillet', array( $this, 'fillet' ) );
+		add_action( 'admin_init', array( $this, 'init' ) );
+
+		if (! is_admin() ) {
+			add_action( 'wp_enqueue_scripts', array( $this, 'fillet_js' ) );
+		}
+	}
+
+	/**
+	 * fillet
+	 *
+	 * Shortcode
+	 *
+	 * @param  string $atts
+	 * @param  string $content
+	 * @author Scott Evans
+	 * @return string
+	 */
+	function fillet($atts, $content = "") {
+
+		global $wpdb, $post;
+
+		// set url sceme on emmbed
+
+		extract( shortcode_atts(
+			array(
+				'url' = '',
+				'width' => '',
+				'hashtag' => ''
+			),
+		$atts, 'fillet' ) );
+
+		// get classes from url
+		// filter classes
+		// add attributes filter
+		// set width and height - overwrite with JS
+		// no border
+		// wrap in o-container?!
+
+		if ($url == '') return;
+
+		$url = set_url_scheme( esc_url( $url ) );
+
+		return $ret;
+
+	}
+
+	/**
+	 * init
+	 *
+	 * Hook into admin init - add tinymce button
+	 *
+	 * @author Scott Evans
+	 * @return void
+	 */
+	function init() {
+
+		# hooks
+		add_action( 'admin_enqueue_scripts', array( $this, 'fillet_mce_css' ) );
+		add_action( 'admin_head', array( $this, 'fillet_mce' ) );
+	}
+
+	/**
+	 * fillet_js
+	 *
+	 * Load the twitter intent JS locally for opening links in small twitter window
+	 *
+	 * @author Scott Evans
+	 * @return void
+	 */
+	function fillet_js() {
+		wp_register_script('fillet-js', plugins_url( '/assets/js/fillet.js' , __FILE__ ), array(), 1);
+		wp_enqueue_script('fillet-js');
+	}
+
+	/**
+	 * fillet_mce_css
+	 *
+	 * Style the editor button
+	 *
+	 * @author Scott Evans
+	 * @return void
+	 */
+	function fillet_mce_css() {
+		wp_enqueue_style( 'fillet', plugins_url( '/assets/css/fillet.css' , __FILE__ ), 'dashicons', 1, 'screen');
+	}
+
+	/**
+	 * fillet_mce
+	 *
+	 * Load the required tools for adding button, check conditions first
+	 *
+	 * @author Scott Evans
+	 * @return void
+	 */
+	function fillet_mce() {
+
+		// check user permissions
+		if ( !current_user_can( 'edit_posts' ) && !current_user_can( 'edit_pages' ) ) {
+			return;
+		}
+
+		// check if WYSIWYG is enabled
+		if ( 'true' == get_user_option( 'rich_editing' ) ) {
+
+			# filters
+			add_filter( 'mce_buttons', array( $this, 'fillet_mce_button' ) );
+			add_filter( 'mce_external_plugins', array( $this, 'fillet_mce_plugin' ) );
+		}
+	}
+
+	/**
+	 * fillet_mce_button
+	 *
+	 * Push a new button on to TinyMCE buttons array
+	 *
+	 * @author Scott Evans
+	 * @param  array $buttons
+	 * @return array
+	 */
+	function fillet_mce_button( $buttons ) {
+		array_push( $buttons, 'fillet' );
+		return $buttons;
+	}
+
+	/**
+	 * fillet_mce_plugin
+	 *
+	 * Add fillet TinyMCE plugin JS
+	 *
+	 * @author Scott Evans
+	 * @param  array $plugins
+	 * @return array
+	 */
+	function fillet_mce_plugin( $plugins ) {
+		$plugins['fillet'] = plugins_url( '/assets/js/fillet-mce.js' , __FILE__ );
+		return $plugins;
+	}
+}
+
+global $fillet;
+$fillet = new fillet();
