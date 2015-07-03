f66d2a8bb66d15aba1a1e3b6979d0dc42cb3db96
Scott Evans <git@scott.ee>
2015-07-03 10:50:08 +0100

    Formatting fixes

--- assets/js/fillet.js
+++ assets/js/fillet.js
@@ -1,34 +1,34 @@
 (function ($) {
 
-    "use strict";
+	"use strict";
 
-    // cftp/fillet iframe auto resizing.  Must go in footer.
+	// fillet iframe auto resizing.  Must go in footer.
 
-    function fillet() {
-        var $container = $('article'),                                  // restrict to actual content
-        $iframes = $container.find('.fillet-embed').find('iframe');     // only iframes added using fillet plugin
+	function fillet() {
+		var $container = $('article'),								// restrict to actual content
+		$iframes = $container.find('.fillet-embed').find('iframe');	// only iframes added using fillet plugin
 
-        $iframes.each(function () {
+		$iframes.each(function () {
 
-            if ($(this).data('aspectRatio') === undefined) {
+			if ($(this).data('aspectRatio') === undefined) {
 
-                $(this).parents('figure')
-                    .data('aspectRatio', this.height / this.width)
-                    .data('originalWidth', this.width)
-                    .data('originalHeight', this.height)
-                    .css('padding-bottom', 100 * (this.height / this.width) + '%' );
+				$(this).parents('figure')
+					.data('aspectRatio', this.height / this.width)
+					.data('originalWidth', this.width)
+					.data('originalHeight', this.height)
+					.css('padding-bottom', 100 * (this.height / this.width) + '%' );
 
-                // remove the hard coded width/height
-                $(this).removeAttr('height').removeAttr('width');
+				// remove the hard coded width/height
+				$(this).removeAttr('height').removeAttr('width');
 
-            }
-        });
-    }
+			}
+		});
+	}
 
-    fillet();
+	fillet();
 
-    // infinite scroll support
-    $(document).on("infinite-scrolled", function(ev) {
-        fillet();
-    });
+	// infinite scroll support
+	$(document).on("infinite-scrolled", function(ev) {
+		fillet();
+	});
 }(jQuery));
